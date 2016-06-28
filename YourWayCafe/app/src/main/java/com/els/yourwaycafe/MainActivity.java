d5b56e452f4b2aaac2e647384d4015f19b20b461
ericaschulz <ericalynnschulz@gmail.com>
2016-06-27 21:19:38 -0500

    update

--- YourWayCafe/app/src/main/java/com/els/yourwaycafe/MainActivity.java
+++ YourWayCafe/app/src/main/java/com/els/yourwaycafe/MainActivity.java
@@ -2,12 +2,32 @@ package com.els.yourwaycafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mStart;
+    Button mPrev;
+    Button mFav;
+    View.OnClickListener mOnClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStart = (Button) findViewById(R.id.btn_start);
+        mFav = (Button) findViewById(R.id.btn_fav);
+        mPrev = (Button) findViewById(R.id.btn_prev);
+
+        /*mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            */
     }
-}
+        }
+
+    
+
